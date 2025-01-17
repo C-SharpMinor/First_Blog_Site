8bbe00cdb3b23d1c13080056219f8a0cfd51321a
Onabajo Oreoluwa <137838886+C-SharpMinor@users.noreply.github.com>
2025-01-17 16:29:46 +0100

    Update themeSlice.js

--- client/src/redux/theme/themeSlice.js
+++ client/src/redux/theme/themeSlice.js
@@ -7,7 +7,7 @@ const initialState = {
 const themeSlice = createSlice({
     name: 'theme',
     initialState,
-    reducers:{
+    reducers:{ //this houses all the actions that can be dispatched to the reducer
         toggleTheme : (state) =>{
             state.theme = state.theme === 'light' ? 'dark' : 'light'
         }
@@ -17,6 +17,14 @@ const themeSlice = createSlice({
 export const {toggleTheme}= themeSlice.actions
 
 export default themeSlice.reducer // so we can add it ot the store
+
+//notice we are separating themeSlice into actions and reducer. even tho in the store we only use themeSlice
+//we are doing because actions and reducer has different functions. The themmeslice.reducer is the default export so it is the one that goes to the store
+//it is what we use when we say 'theme: themeReducer' in the store.js
+//the themeSlice.actions is used in the react code itself to dispatch the action to the reducer
+//eg. dispatch(toggleTheme());
+
+
 
 //after making this we went to the store to add the reducer 
 //then to the main.jsx to cover the App.jsx with a new compoenent(ThemeProvider.jsx) that will
